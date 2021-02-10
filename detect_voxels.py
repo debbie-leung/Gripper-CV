539c40794999b9341d2f29c07cf720d8d7a1f720
Debbie Leung <32530443+debbie-leung@users.noreply.github.com>
2021-02-10 17:30:50 -0500

    Completed simple case of voxel mapping to matrix

--- detect_voxels.py
+++ detect_voxels.py
@@ -7,18 +7,18 @@ import glob
 import sys
 import os
 from checkerboard import detect_checkerboard
-from helper_functions import *
+from tempfile import TemporaryFile
 
 # Detect voxels in simple checkerboard
 filename = sys.argv[1]
 img = cv2.imread(filename)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
-print(hsv)
+#print(hsv)
 
 # Threshold of magenta in HSV space 
 lower_mag = np.array([154,100,100]) 
-upper_mag = np.array([154,255,255]) 
+upper_mag =  np.array([154,255,255])
 
 # preparing the mask to overlay 
 mask = cv2.inRange(hsv, lower_mag, upper_mag) 
@@ -27,17 +27,39 @@ mask = cv2.inRange(hsv, lower_mag, upper_mag)
 # so when multiplied with original image removes all non-magenta regions 
 result = cv2.bitwise_and(img, img, mask = mask) 
   
-cv2.imshow('frame', img) 
-cv2.imshow('mask', mask) 
-cv2.imshow('result', result) 
-      
-cv2.waitKey(0) 
+# cv2.imshow('frame', img) 
+# cv2.imshow('mask', mask) 
+# cv2.imshow('result', result)       
+# cv2.waitKey(0) 
 # cv2.destroyAllWindows() 
 # cap.release() 
 
 # Create empty matrix
-print(img.shape)
-matrix = np.zeros((img.shape[0], img.shape[1]))
+check_x = 6
+check_y = 6
+matrix = np.zeros((check_x, check_y))
+
+voxelx_center = int((img.shape[0] / check_x) / 2)
+voxely_center = int((img.shape[1] / check_y) / 2)
+voxelx_size = int((img.shape[0] / check_x))
+voxely_size = int((img.shape[1] / check_y))
+print("voxelx_center: %d", voxelx_center)
+print("voxely_center: %d", voxely_center)
+print("voxelx_size: %d", voxelx_size)
+print("voxely_size: %d", voxely_size)
+
+for i in range(check_x):
+    for j in range(check_y):
+        if (hsv[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size][0] == 154):
+            cv2.circle(img, tuple([voxelx_center + i*voxelx_size, voxely_center + j*voxely_size]), radius=5, color=(0, 0, 255), thickness=-1)
+            print([voxelx_center + i*voxelx_size, voxely_center + j*voxely_size])
+            matrix[i, j] = 1
+
+cv2.imshow('img',img)
+cv2.waitKey()
+# outfile = TemporaryFile()
+# np.save(outfile, matrix)
+# np.savetxt('test.txt', matrix, delimiter=',')
 print(matrix)
 
 # Get checkerboard corners
@@ -56,6 +78,7 @@ images = glob.glob('*.jpg')
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+"""
 # Find the chess board corners
 ret, corners = cv2.findChessboardCorners(gray, (5,7),None)
 
@@ -65,7 +88,7 @@ if ret == True:
 
     corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
     imgpoints.append(corners2)
-    print(corners2)
+    #print(corners2)
 
     # Draw and display the corners
     img = cv2.drawChessboardCorners(img, (5,7), corners2,ret)
@@ -73,3 +96,4 @@ if ret == True:
     cv2.waitKey()
 
 # cv2.destroyAllWindows()
+"""
