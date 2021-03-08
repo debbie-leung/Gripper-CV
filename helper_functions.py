cc638aabf6d234879b3a81cb26427e80ff08bdcd
Debbie Leung <32530443+debbie-leung@users.noreply.github.com>
2021-03-08 16:04:47 -0500

    Made code cleaner by creating helper functions

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,21 +13,103 @@ def rescaleFrame(frame, scale=0.75):
 
 # Helper function to find largest 4-sided square for checkboard
 def largest_4_sided_contour(processed, show_contours=False):
-    contours, _ = cv2.findContours(
-        processed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # check cv2.RETR_EXTERNAL
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    font = cv2.FONT_HERSHEY_COMPLEX 
+    contours, _ = cv.findContours(
+        processed, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # check cv.RETR_EXTERNAL
+    contours = sorted(contours, key=cv.contourArea, reverse=True)
+    font = cv.FONT_HERSHEY_COMPLEX 
     for cnt in contours[:min(3, len(contours))]:
-        peri = cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, 0.04 * peri, True)
+        peri = cv.arcLength(cnt, True)
+        approx = cv.approxPolyDP(cnt, 0.04 * peri, True)
         if len(approx) == 4:
-            x,y,w,h = cv2.boundingRect(cnt)
+            x,y,w,h = cv.boundingRect(cnt)
             return cnt, x, y, w, h
     return None
     
 def unwarp(img, src, dst, testing):
     h, w = img.shape[:2]
     # use cv2.getPerspectiveTransform() to get M, the transform matrix, and Minv, the inverse
-    M = cv2.getPerspectiveTransform(src, dst)
+    M = cv.getPerspectiveTransform(src, dst)
     # use cv2.warpPerspective() to warp your image to a top-down view
-    warped = cv2.warpPerspective(img, M, (w, h), flags=cv2.INTER_LINEAR)
+    warped = cv.warpPerspective(img, M, (w, h), flags=cv2.INTER_LINEAR)
+
+def watershed(img, mask): # add parameters for tweaking
+    # noise removal
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel, iterations = 5) # default iterations = 5
+
+    # sure background area
+    sure_bg = cv.dilate(opening,kernel,iterations=3) # default iterations = 3
+    # cv.imshow('bg', sure_bg)
+
+    sure_bg_plt = cv.cvtColor(sure_bg, cv.COLOR_BGR2RGB)
+    plt.subplot(1, 3, 1)
+    plt.imshow(sure_bg_plt)
+
+    # Finding sure foreground area
+    dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+    ret, sure_fg = cv.threshold(dist_transform,0.4*dist_transform.max(),255,0) # default parameter = 0.7
+    # cv.imshow('distance transform', dist_transform)
+    # cv.imshow('sure_fg', sure_fg)
+
+    sure_fg_plt = cv.cvtColor(sure_fg, cv.COLOR_BGR2RGB)
+    plt.subplot(1, 3, 2)
+    plt.imshow(sure_fg_plt)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv.subtract(sure_bg,sure_fg)
+
+    # Marker labelling
+    ret, markers = cv.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown==255] = 0
+
+    markers = cv.watershed(img,markers)
+    img[markers == -1] = [255,0,0]
+
+    img_plt = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    plt.subplot(1, 3, 3)
+    plt.imshow(img_plt)
+    plt.show()
+    cv.imshow('img1', img)
+
+    return markers
+
+def centroid_finder(img, markers):
+    # Find centroids of markers (https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/)
+
+    # Output: annotated image with centroids, image with contour fills
+
+    markers1 = markers.astype(np.uint8)
+    ret, m2 = cv.threshold(markers1, 0, 255, cv.THRESH_BINARY|cv.THRESH_OTSU)
+    contours, hierarchy = cv.findContours(m2, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+    centroids = []
+    idx = 0
+
+    filled_contours = np.zeros((img.shape[0], img.shape[1]),np.uint8)
+
+    img2 = img.copy()
+
+    for i, c in enumerate(contours):
+
+        # calculate moments for each contour
+        M = cv.moments(c)
+
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"]) # M["m00"] is the area of the blob
+        cY = int(M["m01"] / M["m00"])
+        # if 120 < M["m00"] < 180:
+        if 1500 < M["m00"] < 3000:
+            centroids.append((cX, cY))
+            cv.circle(img2, (cX, cY), 1, (255, 255, 255), -1)
+            text = str((cX, cY))
+            cv.putText(img2, text, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1, cv.LINE_AA)
+            cv.drawContours(img2, c, -1, (0, 255, 0), 1)
+            cv.drawContours(filled_contours, [c], -1, (255, 255, 255), -1)
+        
+    return centroids, img2, filled_contours 
--- watershed.py
+++ watershed.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import imutils
+import helper_functions as f
 
 # Parameters to be changed
 check_x = 8
@@ -11,142 +12,86 @@ check_y = 8
 # lower_voxel_size
 
 # Read image
-img = cv.imread('cropped.jpg')
+img = cv.imread('missing.jpeg')
 img = imutils.resize(img, width=400)
-img2 = img.copy()
-img3 = img.copy()
 
 # gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 # ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV) 
 
 # Threshold of magenta in HSV space 
 lower_mag = np.array([140,100,20]) # H = 130
 upper_mag = np.array([170,255,255]) # H = 170
 
-# preparing the mask to overlay 
+# Prepare the mask to overlay 
 mask = cv.inRange(hsv, lower_mag, upper_mag) 
+cv.imshow("mask", mask)
 
-# noise removal
-kernel = np.ones((3,3),np.uint8)
-opening = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel, iterations = 5) # default iterations = 3
+markers = f.watershed(img, mask)
 
-# sure background area
-sure_bg = cv.dilate(opening,kernel,iterations=2) # default iterations = 3
-# cv.imshow('bg', sure_bg)
+centroids, img3, filled_contours = f.centroid_finder(img, markers)
 
-sure_bg_plt = cv.cvtColor(sure_bg, cv.COLOR_BGR2RGB)
-plt.subplot(1, 3, 1)
-plt.imshow(sure_bg_plt)
-
-# Finding sure foreground area
-dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
-ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
-# cv.imshow('distance transform', dist_transform)
-# cv.imshow('sure_fg', sure_fg)
-
-sure_fg_plt = cv.cvtColor(sure_fg, cv.COLOR_BGR2RGB)
-plt.subplot(1, 3, 2)
-plt.imshow(sure_fg_plt)
-
-# Finding unknown region
-sure_fg = np.uint8(sure_fg)
-unknown = cv.subtract(sure_bg,sure_fg)
-
-# Marker labelling
-ret, markers = cv.connectedComponents(sure_fg)
-
-# Add one to all labels so that sure background is not 0, but 1
-markers = markers+1
-
-# Now, mark the region of unknown with zero
-markers[unknown==255] = 0
-
-markers = cv.watershed(img,markers)
-img[markers == -1] = [255,0,0]
-
-img_plt = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-plt.subplot(1, 3, 3)
-plt.imshow(img_plt)
-plt.show()
-img = imutils.resize(img, width=400)
-cv.imshow('img1', img)
-
-# Find centroids of markers (https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/)
-# img2 = img.copy()
-markers1 = markers.astype(np.uint8)
-ret, m2 = cv.threshold(markers1, 0, 255, cv.THRESH_BINARY|cv.THRESH_OTSU)
-contours, hierarchy = cv.findContours(m2, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-
-centroids = []
-idx = 0
-
-for i, c in enumerate(contours):
-
-    # calculate moments for each contour
-    M = cv.moments(c)
-
-    # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"]) # M["m00"] is the area of the blob
-    cY = int(M["m01"] / M["m00"])
-    # if 120 < M["m00"] < 180:
-    if 1500 < M["m00"] < 3000:
-        centroids.append((cX, cY))
-        cv.circle(img2, (cX, cY), 1, (255, 255, 255), -1)
-        text = str((cX, cY))
-        cv.putText(img3, text, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1, cv.LINE_AA)
-        cv.drawContours(img3, c, -1, (0, 255, 0), 1)
-
-# print(len(centroids))
-centroids = set(centroids)
-# print("length")
-print(len(centroids))
-centroids = sorted(list(centroids), key=lambda element: element[1])
-# np.savetxt("centroids.txt", np.array(centroids), fmt='%d')
-# print("numpy")
-# centroids_reshaped = np.array(centroids).reshape((8,8))
-# print(np.array(centroids))
-# img2 = imutils.resize(img2, width=400)
-# cv.imshow('markers1', markers1)
-cv.imshow('img2', img2)
-# img3 = imutils.resize(img3, width=400)
-cv.imshow('img3', img3)
-cv.waitKey()
+cv.imshow("img3", img3)
 
 # Output boolean array
 pixelx_per_mm = img.shape[0]/(check_x*3) # assume width and height are perfectly square
+pixely_per_mm = img.shape[1]/(check_y*3)
 grid = np.zeros((check_x,check_y))
-# Hardcode finding centroid
-# voxelx_center = int((img.shape[0] / check_x) / 2)
-# voxely_center = int((img.shape[1] / check_y) / 2)
-# voxelx_size = int((img.shape[0] / check_x))
-# voxely_size = int((img.shape[1] / check_y))
+print(grid)
 
-# print("voxelx_center: ", voxelx_center)
-# print("voxely_center: ", voxely_center)
-# print("voxelx_size: ", voxelx_size)
-# print("voxely_size: ", voxely_size)
+for i in range(check_x):
+    for j in range(check_y):
+        # if (hsv[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size][0] == 154):
+        if filled_contours[int(1.5*pixelx_per_mm + i*3*pixelx_per_mm), int(1.5*pixely_per_mm + j*3*pixely_per_mm)]: 
+            cv.circle(img4, tuple([int(1.5*pixelx_per_mm + i*3*pixelx_per_mm), int(1.5*pixely_per_mm + j*3*pixely_per_mm)]), radius=2, color=(0, 255, 0), thickness=-1)
+            grid[i, j] = 2
+# print(grid)
 
-# for i in range(check_x):
-#     for j in range(check_y):
-#         # if (hsv[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size][0] == 154):
-#         if mask[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size]: 
-#             cv2.circle(img, tuple([voxelx_center + i*voxelx_size, voxely_center + j*voxely_size]), radius=5, color=(0, 0, 255), thickness=-1)
-#             grid[i, j] = 1
+voxelx_center = int((img.shape[0] / check_x) / 2)
+voxely_center = int((img.shape[1] / check_y) / 2)
+voxelx_size = int((img.shape[0] / check_x))
+voxely_size = int((img.shape[1] / check_y))
+
+for i in range(check_x):
+    for j in range(check_y):
+        # if (hsv[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size][0] == 154):
+        if mask[voxelx_center + i*voxelx_size, voxely_center + j*voxely_size]: 
+            cv.circle(img5, tuple([voxelx_center + i*voxelx_size, voxely_center + j*voxely_size]), radius=2, color=(255, 0, 0), thickness=-1)
+            grid[i, j] = 1
+
+cv.imshow("img4", img4)
+cv.imshow("filled", filled_contours)
+# cv.imshow('img5',img5)
+cv.waitKey()
+
+# print(grid)
 
 # Output array of centroids
-pixely_per_mm = img.shape[1]/(check_y*3)
 centroid_grid = np.zeros((check_x,check_y), dtype='i,i')
 
-low_y = 0
-for j in range(voxel_num - 1):
-    # slice centroids list to get each row for sorting
-    #  sorted(centroids[centroids[:, 1] > 50], key=lambda element: element[0])
-    high_y = 0 + (j+1)*pixely_per_mm
-    row = x for x in centroids if low_y <= centroids[:, 1] <= high_y)
-    low_y = high_y
-    row = sorted(row, , key=lambda element: element[0])
+centroids = set(centroids)
+# print("length")
+# print(len(centroids))
+centroids = sorted(list(centroids), key=lambda element: element[1])
+# print(centroids)
+# np.savetxt("centroids.txt", np.array(centroids), fmt='%d')
+# print("numpy")
+centroids = np.array(centroids)
+# print(centroids)
+
+# low_y = 0
+# for j in range(check_y - 1):
+#     # slice centroids list to get each row for sorting
+#     #  sorted(centroids[centroids[:, 1] > 50], key=lambda element: element[0])
+#     high_y = 0 + (j+1)*pixely_per_mm
+#     print(low_y)
+#     print(high_y)
+#     row = centroids[centroids[:, 1] >= low_y]
+#     row = row[row[:, 1] <= high_y]
+#     # row = [x for x in centroids_reshaped if low_y <= centroids_reshaped[:,1] <= high_y]
+#     low_y = high_y
+#     row = sorted(row, key=lambda element: element[0])
+#     print(list(row))
     
     
     
